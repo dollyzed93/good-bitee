654fc280391e1e465cdf8d17e38cad75cee80aed
dollyg93 <dollygarg137@gmail.com>
2019-01-24 15:33:29 +0530

    changes

--- common/models/people.js
+++ common/models/people.js
@@ -32,18 +32,21 @@ module.exports = function(People) {
     People.app.models.Container.imageUpload(ctx.req, ctx.res,
       {container: 'profileImages'}, function(err, success) {
         if (err) return next(err, null);
-        if (success.data) {
-          success.data = JSON.parse(success.data);
-          const user = success.data;
-          if (!user.customerId) {
-            if (!user.fullName || !user.mobile ||
-              !user.email || !user.password || !user.realm) {
-              return next(new Error('Incomplete data is provided'));
-            } else {
-              ctx.req.user = success;
-              next();
-            }
-          }
+        // success.data
+        if (success) {
+          ctx.req.user = success;
+          next();
+          // success.data = JSON.parse(success.data);
+          // const user = success.data;
+          // if (!user.customerId) {
+          //   if (!user.fullName || !user.mobile ||
+          //     !user.email || !user.password || !user.realm) {
+          //     return next(new Error('Incomplete data is provided'));
+          //   } else {
+          //     ctx.req.user = success;
+          //     next();
+          //   }
+          // }
         } else {
           return next(new Error('data not found'));
         }
@@ -54,7 +57,8 @@ module.exports = function(People) {
   People.signup = (req, res, cb) => {
     let obj = req.user;
     console.log(obj);
-    let query = {where: {or: []}};
+    cb(null, obj);
+    /*let query = {where: {or: []}};
     if (obj.data.email) {
       query.where.or.push({email: obj.data.email});
     }
@@ -164,12 +168,13 @@ module.exports = function(People) {
           }
         }
       }
-    });
+    });*/
   };
 
   People.afterRemote('signup', function(ctx, instance, next) {
     console.log('> user.afterRemote triggered');
-    let userInstance = instance.success.data;
+    next();
+    /* let userInstance = instance.success.data;
     userInstance.addRole(userInstance.realm, function(err, success) {
       if (err) next(new Error(err));
       if (!userInstance.mobileVerified) {
@@ -213,7 +218,7 @@ module.exports = function(People) {
       } else {
         next();
       }
-    });
+    });*/
   });
 
   People.afterRemoteError('signup', function(ctx, next) {
