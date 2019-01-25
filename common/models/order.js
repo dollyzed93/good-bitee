13d7b72e856495990bdad399695b4b2cd8cf888e
dollyg93 <dollygarg137@gmail.com>
2019-01-25 11:07:24 +0530

    removed incomplete model

--- common/models/order.js
+++ common/models/order.js
@@ -1,8 +1,4 @@
 'use strict';
 
 module.exports = function(Order) {
-  Order.validatesInclusionOf('status', {in:
-  ['pending', 'approved', 'cancelled',
-    'deliveryBoyAssigned', 'completed',
-    'food prepared', 'on the way']});
 };
